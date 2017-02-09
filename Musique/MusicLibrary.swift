f5efd6256b67850eb0cc41f88dc8ff0fb1f29aa0
Badini <badini@Badinis-Mac.local>
2017-02-09 14:29:33 +0100

    update gradur img

--- Musique/MusicLibrary.swift
+++ Musique/MusicLibrary.swift
@@ -11,6 +11,6 @@ import Foundation
 struct MusicLibrary {
     
     
-    var library = [["titre":"Chikita", "artist":"jul", "index":"0", "coverImage":"jul"],  ["titre":"Le diable habille en prada","artist":"soprano", "index":"1", "coverImage":"soprano"], ["titre":"PPT ", "artist":"gradur", "index":"2", "coverImage":"GRADUR"],["titre":"Champions League", "artist":"mhd", "index":"3", "coverImage":"mhd"] ]
+    var library = [["titre":"Chikita", "artist":"jul", "index":"0", "coverImage":"jul"],  ["titre":"Le diable habille en prada","artist":"soprano", "index":"1", "coverImage":"soprano"], ["titre":"PPT ", "artist":"gradur", "index":"2", "coverImage":"gradur"],["titre":"Champions League", "artist":"mhd", "index":"3", "coverImage":"mhd"] ]
     
 }
